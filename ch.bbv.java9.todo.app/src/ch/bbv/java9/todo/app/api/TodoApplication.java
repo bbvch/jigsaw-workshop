006b6fe809c2c0a443ae73d44e6c5d955b17fcc1
Daniel Tiefenauer <git@tiefenauer.info>
2017-01-03 16:05:35 +0100

    add solutions for exercise-01

---
+++ ch.bbv.java9.todo.app/src/ch/bbv/java9/todo/app/api/TodoApplication.java
@@ -0,0 +1,11 @@
+package ch.bbv.java9.todo.app.api;
+
+/**
+ * Created by Daniel on 03.01.2017.
+ */
+public class TodoApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ ch.bbv.java9.todo.app/src/module-info.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Daniel on 03.01.2017.
+ */
+module ch.bbv.java9.todo.app {
+    exports ch.bbv.java9.todo.app.api;
+}
