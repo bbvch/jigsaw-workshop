f71a824d9a015c8ce9aa7e65d209d25c19392ce9
Daniel Tiefenauer <git@tiefenauer.info>
2017-01-03 17:29:37 +0100

    add implementation for TodoItem

--- ch.bbv.java9.todo.model/ch/bbv/java9/todo/model/entity/TodoItem.java
+++ ch.bbv.java9.todo.model/ch/bbv/java9/todo/model/entity/TodoItem.java
@@ -4,4 +4,15 @@ package ch.bbv.java9.todo.model.entity;
  * Created by Daniel on 03.01.2017.
  */
 public class TodoItem {
+
+    private String message = null;
+
+    public TodoItem(String message){
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
 }
