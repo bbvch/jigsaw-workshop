78121e63766292a6c97bd0ba1a7676991268f233
Daniel Tiefenauer <git@tiefenauer.info>
2017-01-03 17:45:44 +0100

    add solutions for exercise-06

---
+++ package.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+echo packaging/copying modules...
+$J9/jar -c -f out/app.jar --module-version=1.0 -e ch.bbv.java9.todo.app.api.TodoApplication -C mods/ch.bbv.java9.todo.app .
+$J9/jar -c -f out/model.jar --module-version=1.0 -C mods/ch.bbv.java9.todo.model .
+
+echo ... done!
---
+++ run-module.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+$J9/java -p out -m ch.bbv.java9.todo.app "Do the laundry" "Go shopping" "Call Mum"
