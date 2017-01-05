a740a92d47b629dcebe3f9ae9f546cbba62e16ab
Daniel Tiefenauer <git@tiefenauer.info>
2017-01-05 13:26:12 +0100

    sepaseparate runs to test Importer functionality in run-modular-jar.sh

--- run-modular-jar.sh
+++ run-modular-jar.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
-echo Running modular jar...
-$J9/java -p out -m ch.bbv.java9.todo.app "Write Birthday Card" "Check Email" "Pay Taxes"
+echo Running modular jar with three arguments...
+$J9/java -p "out;lib" -m ch.bbv.java9.todo.app "Write Birthday Card" "Check Email" "Pay Taxes"
+echo
+echo Running TodoApplication with one argument...
+$J9/java -p "out;lib" -m ch.bbv.java9.todo.app items
 echo ...done!
