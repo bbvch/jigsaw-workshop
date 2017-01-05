88cdee593a2f359739235df5497c4f371abceb7e
Daniel Tiefenauer <git@tiefenauer.info>
2017-01-05 11:08:28 +0100

    rename TodoDefaultImporter move CSVParser member

--- ch.bbv.java9.todo.util/ch/bbv/java9/todo/util/internal/TodoCLIImporter.java
+++ ch.bbv.java9.todo.util/ch/bbv/java9/todo/util/internal/TodoCLIImporter.java
@@ -2,16 +2,15 @@ package ch.bbv.java9.todo.util.internal;
 
 import ch.bbv.java9.todo.model.entity.TodoItem;
 import ch.bbv.java9.todo.util.api.TodoImporter;
-import org.apache.commons.csv.CSVParser;
 
 import java.util.List;
 
 /**
  * Created by Daniel on 03.01.2017.
  */
-public class TodoDefaultImporter implements TodoImporter {
+public class TodoCLIImporter implements TodoImporter {
 
-    private CSVParser records = null;
+
 
     @Override
     public List<TodoItem> importItems() {
--- ch.bbv.java9.todo.util/ch/bbv/java9/todo/util/internal/TodoCSVImporter.java
+++ ch.bbv.java9.todo.util/ch/bbv/java9/todo/util/internal/TodoCSVImporter.java
@@ -2,6 +2,7 @@ package ch.bbv.java9.todo.util.internal;
 
 import ch.bbv.java9.todo.model.entity.TodoItem;
 import ch.bbv.java9.todo.util.api.TodoImporter;
+import org.apache.commons.csv.CSVParser;
 
 import java.util.List;
 
@@ -9,6 +10,8 @@ import java.util.List;
  * Created by Daniel on 03.01.2017.
  */
 public class TodoCSVImporter implements TodoImporter {
+
+    private CSVParser records = null;
 
     @Override
     public List<TodoItem> importItems() {
