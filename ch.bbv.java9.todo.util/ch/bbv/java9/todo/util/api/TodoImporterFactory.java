d06632add806495fd26ed3eb05d659bdb5b21926
Daniel Tiefenauer <git@tiefenauer.info>
2017-01-05 10:58:09 +0100

    add factory method stub

--- ch.bbv.java9.todo.util/ch/bbv/java9/todo/util/api/TodoImporterFactory.java
+++ ch.bbv.java9.todo.util/ch/bbv/java9/todo/util/api/TodoImporterFactory.java
@@ -4,4 +4,8 @@ package ch.bbv.java9.todo.util.api;
  * Created by Daniel on 03.01.2017.
  */
 public class TodoImporterFactory {
+
+    public static TodoImporter from(String[] args){
+        return null;
+    }
 }
