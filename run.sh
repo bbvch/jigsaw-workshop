80d01d0aeff0883c2a8f57b79dad6013c4ce92e6
Daniel Tiefenauer <git@tiefenauer.info>
2017-01-05 13:19:19 +0100

    change TodoItems in CLI arguments

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 echo Running TodoApplication...
-$J9/java -p mods -m ch.bbv.java9.todo.app/ch.bbv.java9.todo.app.api.TodoApplication "Do the laundry" "Go shopping" "Call Mum"
+$J9/java -p mods -m ch.bbv.java9.todo.app/ch.bbv.java9.todo.app.api.TodoApplication "Write Birthday Card" "Check Email" "Pay Taxes"
 
 echo "... done!"
